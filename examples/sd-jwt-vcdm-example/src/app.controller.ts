eed58c7779bff945d3adff211b3ebfd47a3d2105
Lukas.J.Han <lukas.j.han@gmail.com>
2025-02-06 18:05:28 +0900

    feat: add testing jades logic

--- examples/sd-jwt-vcdm-example/src/app.controller.ts
+++ examples/sd-jwt-vcdm-example/src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('credential')
+  async credential() {
+    const credential = await this.appService.credential();
+    return {
+      credentials: [
+        {
+          credential,
+        },
+      ],
+    };
+  }
 }
--- examples/sd-jwt-vcdm-example/src/app.service.ts
+++ examples/sd-jwt-vcdm-example/src/app.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
 
+import {
+  Token,
+  ProtectedHeaders,
+  parseCerts,
+  generateX5c,
+} from '@xevolab/jades';
+import { createPrivateKey } from 'crypto';
+import * as fs from 'fs';
+
 @Injectable()
-export class AppService {}
+export class AppService {
+  async credential() {
+    const payload = {
+      hello: 'world',
+    };
+
+    const key = createPrivateKey({
+      key: fs.readFileSync('private-key.pem', 'utf8'),
+      format: 'pem',
+      type: 'pkcs1',
+    });
+    const certs = parseCerts(fs.readFileSync('certificate.pem', 'utf8'));
+
+    const jades = new Token(payload);
+
+    jades.setProtectedHeaders(
+      new ProtectedHeaders({
+        x5c: generateX5c(certs),
+      }),
+    );
+    jades.sign('RS256', key);
+
+    const token = jades.toString();
+    console.log(token);
+    return token;
+  }
+}
